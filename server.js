06e8e7078c742d48349c0da8995bc5f7790222b2
hiiragi1208 <d7nwyg94nv@privaterelay.appleid.com>
2026-05-25 04:51:24 +0000

    fix port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { URL } = require("url");
 const zlib = require("zlib");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
